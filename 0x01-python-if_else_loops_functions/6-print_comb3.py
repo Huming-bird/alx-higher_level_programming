54b45adf1b9b4b70f7f47a5e2e5ed7585cb0cef9
Huming-bird <sonugaahmed@gmail.com>
2023-03-07 08:05:04 -0800

    python conditionals

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num !=89 and int(num / 10) != num % 10 and int(num / 10) < num % 10:
+    if num != 89 and int(num / 10) != num % 10 and int(num / 10) < num % 10:
         print("{}{}, ".format(int(num / 10), num % 10), end="")
     if (num == 89):
         print(f"{num}")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
-#!/usr/bin/python3
 def print_last_digit(number):
-    return int(str(number)[-1])
+    if number >= 0:
+        ld = number % 10
+    else:
+        ld = number % -10
+        ld *= -1
+    print("{:d}".format(ld), end='')
+    return (ld)
