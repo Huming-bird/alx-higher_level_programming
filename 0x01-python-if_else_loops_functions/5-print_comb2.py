fe01900cf7b2343e96c579d3d2a5cc5513afd60b
Huming-bird <sonugaahmed@gmail.com>
2023-03-07 07:29:12 -0800

    python conditionals

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for number in range(100):
     if number != 99:
-        print("{}, ".format('0' + str(number) if number < 10 else number), end='')
+        print("{}, ".format('0' + str(number) \
+if number < 10 else number), end='')
     else:
         print("{}".format(number))
