61d2b0d699dc74ac286b961699d2f27d59c1d120
Huming-bird <sonugaahmed@gmail.com>
2023-03-09 07:41:00 -0800

    uodates to 0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import add_0
+    """print the addition of 1 and 2"""
+import add_0
 
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    ans = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, ans))
+ans = add_0.add(a, b)
+print("{} + {} = {}".format(a, b, ans))
