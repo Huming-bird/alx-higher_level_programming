b604c219433bc4a98052bb4bd3e20bb401b130d2
Huming-bird <sonugaahmed@gmail.com>
2023-03-09 07:46:36 -0800

    updates

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     """print the addition of 1 and 2"""
-import add_0
-
+from add_0 import add
 a = 1
 b = 2
-
-ans = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, ans))
+print("{} + {} = {}".format(a, b, add(a, b)))
