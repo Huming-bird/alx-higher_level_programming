04106a7dccbb6c861d954395036539412a6aed9f
Huming-bird <sonugaahmed@gmail.com>
2023-03-10 06:38:43 -0800

    python data structures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    for xter in my_string:
+        if xter == 'c'.islower() or xter == 'c'.isupper():
+            pass
+        else:
+            print("{}".format(xter), end='')
+    print('')
