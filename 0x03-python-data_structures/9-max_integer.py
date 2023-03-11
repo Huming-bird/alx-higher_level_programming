b2a666555092a7dd4f6e092b05f11a78397d4ea1
Huming-bird <sonugaahmed@gmail.com>
2023-03-11 07:24:00 -0800

    updates

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        maxi = "None"
+    else:
+        maxi = 0
+        for i in my_list:
+            if i > maxi:
+                maxi = i
+    return maxi
