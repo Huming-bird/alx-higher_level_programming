183ba6b4175ea3ba327f0e822150055f60bd1dac
Huming-bird <sonugaahmed@gmail.com>
2023-03-15 08:38:57 -0700

    updates

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            my_list[my_list.index(i)] = replace
-    return my_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,9 +2,6 @@
 
 def print_sorted_dictionary(a_dictionary):
     holder = sorted(a_dictionary.keys())
-    new = {}
 
     for keys in holder:
-        new[keys] = a_dictionary[keys]
-    for key, value in new:
-        print(f"{key}: {value}")
+        print('{}: {}'.format(keys, a_dictionary.get(keys)))
