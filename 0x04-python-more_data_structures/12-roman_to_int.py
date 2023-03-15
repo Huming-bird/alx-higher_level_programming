04cc5e9d96f810032dee441995e6e2c3367e8cf9
Huming-bird <sonugaahmed@gmail.com>
2023-03-15 10:19:46 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 
 def roman_to_int(roman_string):
     numerals = { 
-        'I': 1
-        'V': 5
-        'X': 10
-        'L': 50
-        'C': 100
-        'D': 500
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
         'M': 1000
     }
     total = 0
