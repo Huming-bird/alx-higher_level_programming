9e5328a5a7e16e8e39f07b27b5b0f0f4626bfb05
Huming-bird <sonugaahmed@gmail.com>
2023-04-03 12:12:19 -0700

    OOP in Python

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -89,7 +89,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() == max(rect_1.area(), rect_2.area())\
             else rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         return cls(size, size)
