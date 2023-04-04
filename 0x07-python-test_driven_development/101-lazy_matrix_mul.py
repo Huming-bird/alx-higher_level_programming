73842d8891a6416c730a91fa6d0a85fe829aff39
Huming-bird <sonugaahmed@gmail.com>
2023-04-04 13:42:05 -0700

    updates

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -5,12 +5,13 @@
 
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """
     the input is a list of list which will be converted
     to a numpy array
     """
-    
+
     m_a = np.array(m_a)
     m_b = np.array(m_b)
 
