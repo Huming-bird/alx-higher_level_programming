0ac7a279c1e4244da0028c2b9887b5203259ecf1
Huming-bird <sonugaahmed@gmail.com>
2023-04-12 11:13:49 -0700

    IO in python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
     
     try:
-        my_obj = load_from_json_file(filename)
-    except Exception:
+        my_obj = load_from_json_file("add_item.json")
+    except FileNotFoundError:
         my_obj = []
         my_obj.extend(sys.argv[1:])
-        save_to_json_file(my_obj, add_item.json)
+        save_to_json_file(my_obj, 'add_item.json')
