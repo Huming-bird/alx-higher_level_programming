e1e27cd5a44fd108824cb694bc2c960f2e84dcb1
Huming-bird <sonugaahmed@gmail.com>
2023-05-17 09:45:13 -0700

    more SQL queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,7 @@
 -- this script creates a DB and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS cities(
 	`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
