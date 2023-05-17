d0b017940d77f316598b4fab7027fa7f6e01ffec
Huming-bird <sonugaahmed@gmail.com>
2023-05-17 08:59:13 -0700

    more SQL queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,4 @@
 -- this sccript import the DB dump from a DB
-USE hbtn_0d_tvshows;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,4 @@
 -- this script lists all citites of CA found in the DB
-USE hbtn_0d_usa;
-
 SELECT * FROM cities
 WHERE state_id = (
   SELECT id FROM states
