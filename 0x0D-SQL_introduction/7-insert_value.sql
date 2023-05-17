4306b45e16f1f6751ce7b5fcf38b436fefcc788e
Huming-bird <sonugaahmed@gmail.com>
2023-05-17 08:14:35 -0700

    SQL introductions

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- this script inserts a new role to a table
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "BEST SCHOOL");
+INSERT INTO first_table(`id`, `name`)
+VALUES (89, "BEST SCHOOL");
