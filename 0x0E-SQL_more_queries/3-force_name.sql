cfba40863a3cc2f3974d4961969f5d5a177c3b64
Huming-bird <sonugaahmed@gmail.com>
2023-05-17 07:07:36 -0700

    more SQL queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,2 @@
 -- this script creates a table on myserver
-CREATE TABLE IF NOT EXISTS 'force_name'(
-	`id` INT,
-	`name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS 'force_name'(id INT,name VARCHAR(256) NOT NULL);
