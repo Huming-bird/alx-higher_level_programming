322f7ac8c005ff36c5b313a0acf3f522c17c7e83
Huming-bird <sonugaahmed@gmail.com>
2023-05-17 09:38:52 -0700

    more SQL queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- this script lists all citites of CA found in the DB
-SELECT * FROM cities
+SELECT `id`, `name` FROM cities
 WHERE state_id = (
   SELECT id FROM states
   WHERE name = 'California'
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- this script lists all cities contained in the DB
-SELECT cities.id, cities.name, states.name FROM cities, states
-WHERE cities.states_id = states.id
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id ASC;
