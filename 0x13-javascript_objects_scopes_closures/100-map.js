6e6f134cf647f443152bf8870393135cdd6441b4
Huming-bird <sonugaahmed@gmail.com>
2023-06-15 16:29:38 +0000

    updates to 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const {list} = require('./100-data.js');
+const { list } = require('./100-data.js');
 
 const newList = list.map((x, index) => x * index);
 
 console.log(list);
 console.log(newList);
-
