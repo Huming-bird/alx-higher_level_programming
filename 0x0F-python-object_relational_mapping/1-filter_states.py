0c780dcc1009550cabd101ea2747f4efc3ba5552
Huming-bird <sonugaahmed@gmail.com>
2023-06-17 02:17:30 -0700

    ORMs in Python ALXSWE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ def main():
 
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name CONTAINS "N" ORDER BY id ASC')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%N%' ORDER BY id ASC")
     result = cur.fetchall()
     for row in result:
         print(row)
