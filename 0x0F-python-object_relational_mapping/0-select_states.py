b0f07f89ab20a8bc08e6c93418a6b401e1b660e5
Huming-bird <sonugaahmed@gmail.com>
2023-06-17 00:52:33 -0700

    ORMs in Python ALXSWE

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,14 +5,20 @@
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute('SELECT * FROM states ORDER BY id ASC')
-result = cur.fetchall()
-for row in result:
-    print(row)
 
-cur.close()
-db.close()
+def main():
+    ''' this function executes when called '''
 
-#if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    main()
