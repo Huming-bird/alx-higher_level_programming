e3654d28170bb4905f43573d5176dcf6253f559b
Huming-bird <sonugaahmed@gmail.com>
2023-06-17 00:38:05 -0700

    ORM in Python ALXSWE

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+''' This script lists alll staes from hbtn DB '''
+
+import sys
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY id ASC')
+result = cur.fetchall()
+for row in result:
+    print(row)
+
+cur.close()
+db.close()
+
+#if __name__ == '__main__':
