0790cdb952ed50d859fdfe33e62967a8af43c75b
Huming-bird <sonugaahmed@gmail.com>
2023-06-30 08:50:14 -0700

    updated scripts for task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script gets only response code without using |
-grep -Eo "[0-9]+" <<< $(curl -sw %{http_code} 0.0.0.0:5000/route_3)
+grep -Eo "[0-9]+" <<< $(curl -sw %{http_code} "$1")
