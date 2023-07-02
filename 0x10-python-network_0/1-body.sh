07a02f65cf4a6a772f829d6dbcd5823ecc32c3ee
Huming-bird <sonugaahmed@gmail.com>
2023-07-02 13:38:16 -0700

    updates to tasks

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this sccript returns the body of a 200code response
-res=$(curl -sI "$1" | grep 'HTTP' | cut -d' ' -f2); if [[ $res -eq 200 ]]; then curl -s "$1" -o t && cat t; fi
+curl -sL "$1"
