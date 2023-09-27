c42f4a9d6c06a2505bf3287c60ee5879ea18fabc
Huming-bird <ahmedolalekan2@gmail.com>
2023-09-27 11:23:07 -0700

    updates

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
+const filename = process.argv[2];
 
-fs.readFile(file, 'utf-8', (error, content) => {
+fs.readFile(filename, 'utf-8', (error, content) => {
   if (error) {
     console.log(error);
   } else {
